4215a8b8b1b446d66830da14cc362ce739c00a24
eneas-hsn <eneashsn@gmail.com>
2020-05-18 13:56:10 -0300

    rest

---
+++ restSpread.js
@@ -0,0 +1,16 @@
+// //rest
+// const convidado={nome:'eneas', idade:16, endereço:'av x, n39'}
+
+// const{nome, ...rest}=convidado
+
+// console.log(nome,rest)
+
+const convidados=[
+    {nome:'eneas', idade:16, endereço:'av x, 32'},
+    {nome:'eneias', idade:18, endereço:'av x, 320'},
+    {nome:'enea', idade:17, endereço:'av x, 3'},
+    {nome:'enesio', idade:19, endereço:'av x, 2'}
+]
+const [eneas, eneias,...rest]=convidados
+console.log(eneas)
+console.log(rest)
