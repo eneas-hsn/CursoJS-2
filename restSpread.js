cb371c0b41c9bd44ba0258b5a0d12c6f9b12a53e
eneas-hsn <eneashsn@gmail.com>
2020-05-18 14:00:42 -0300

    spread

--- restSpread.js
+++ restSpread.js
@@ -5,12 +5,22 @@
 
 // console.log(nome,rest)
 
-const convidados=[
-    {nome:'eneas', idade:16, endereço:'av x, 32'},
-    {nome:'eneias', idade:18, endereço:'av x, 320'},
-    {nome:'enea', idade:17, endereço:'av x, 3'},
-    {nome:'enesio', idade:19, endereço:'av x, 2'}
-]
-const [eneas, eneias,...rest]=convidados
-console.log(eneas)
-console.log(rest)
+// const convidados=[
+//  {nome:'eneas', idade:16, endereço:'av x, 32'},
+//     {nome:'eneias', idade:18, endereço:'av x, 320'},
+//     {nome:'enea', idade:17, endereço:'av x, 3'},
+//     {nome:'enesio', idade:19, endereço:'av x, 2'}
+// ]
+// const [eneas, eneias,...rest]=convidados
+// console.log(eneas)
+// console.log(rest)
+
+
+// const arr1=[1,2,3]
+// const arr2=[4,5,6]
+// const arr3=[...arr1,...arr2]
+// console.log(arr3)
+
+const convidados={nome:'eneas', idade:16, endereço:'av x, 32'}
+    const novo={...convidados, idade:11}
+    console.log(novo)
