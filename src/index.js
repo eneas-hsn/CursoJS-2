7bafef530a90a8abbffc936d6b9010a53bcb617b
eneas-hsn <eneashsn@gmail.com>
2020-05-22 23:17:55 -0300

    promise

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const promisse=()=>{
+const promise=()=>{
     return new Promise((resolve ,reject)=>{setTimeout(()=>{
         resolve('pronto')
     },2000)})
 }
 console.log('start')
-promisse().then(res=>{
+promise().then(res=>{
     console.log('exem1', res)
 })
 async function outrapormisse() {
-    const res= await promisse()
+    const res= await promise()
     console.log('ex',res)
 }
  const xpromise=async()=> {
-    const res= await promisse()
+    const res= await promise()
     console.log('ex3',res)
 }
 outrapormisse()
